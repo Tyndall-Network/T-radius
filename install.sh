bacc6f43d8a2586cf911c0a0dfbc84491a10f41e
Bram-Wel <welbram@gmail.com>
2023-08-11 13:12:36 +0300

    Install freeradius.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Install freeradius on ubuntu jammy-- 22.04
+# Add NetworkRadius[https://networkradius.com/packages/] PGP key
+install -d -o root -g root -m 0755 /etc/apt/keyrings
+curl -s 'https://packages.networkradius.com/pgp/packages%40networkradius.com' | \
+  sudo tee /etc/apt/keyrings/packages.networkradius.com.asc > /dev/null
+
+# APT preferences
+printf 'Package: /freeradius/\nPin: origin "packages.networkradius.com"\nPin-Priority: 999\n' |
+  sudo tee /etc/apt/preferences.d/networkradius > /dev/null
+
+# APT sources list
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.networkradius.com.asc] http://packages.networkradius.com/freeradius-3.2/ubuntu/jammy jammy main" | \
+    sudo tee /etc/apt/sources.list.d/networkradius.list > /dev/null
+
+# Update APT database & install packages
+sudo apt-get update
+sudo apt-get install freeradius
+
+# Check out networkradius(https://networkradius.com/packages/#fr32) for other distros and OS's.
