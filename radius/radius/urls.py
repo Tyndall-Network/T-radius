e150c7efc037852f76ef7e883119d6843465a28f
Bram-Wel <welbram@gmail.com>
2023-12-20 22:16:07 +0300

    Remove dashboard

--- radius/radius/urls.py
+++ radius/radius/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from dashboard.views import home_view
+from users.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
